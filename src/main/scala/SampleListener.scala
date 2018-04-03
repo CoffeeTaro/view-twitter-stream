e4374f9b95713d053fb3157c7119a03b089d8742
Asamin <CoffeeTaro@users.noreply.github.com>
2018-04-03 22:40:01 +0900

    fix: コメントの再編集

--- src/main/scala/SampleListener.scala
+++ src/main/scala/SampleListener.scala
@@ -6,17 +6,17 @@ class SampleListener extends StatusListener {
   }
 
   override def onDeletionNotice(notice: StatusDeletionNotice): Unit = {
-    // println(notice)
+
   }
   override def onScrubGeo(x1: Long, x2: Long): Unit = {}
   override def onException(e: Exception): Unit = {
     e.printStackTrace()
   }
   override def onStallWarning(stw: StallWarning): Unit = {
-    // println(stw)
+
   }
   override def onTrackLimitationNotice(x: Int): Unit = {
-    // println(x)
+
   }
 
 }
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -4,6 +4,7 @@ object main {
   def main(args: Array[String]): Unit = {
     // twitter streaming APIを起動
     val twitterStream = new TwitterStreamFactory().getInstance()
+    // Listenerの作成
     val listener = new SampleListener()
     twitterStream.addListener(listener)
 
